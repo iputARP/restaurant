67e2b26e79552c6ed58137d441b9c57240a1d59e
taishin <ok220259@oks.iput.ac.jp>
2024-04-01 13:34:08 +0900

    継承

---
+++ src/image.py
@@ -0,0 +1,19 @@
+#! /usr/bin/env python3
+
+import rospy
+from sensor_msgs.msg import Image
+from gpsr.srv import QRCodeReader,QRCodeReaderRequest
+class ImageData:
+    def __init__(self):
+        rospy.Subscriber("/hsrb/head_center_camera/image_raw",Image,self.callback)
+
+
+    def callback(self,msg):
+        pass
+
+    def register_service(self):
+        pass
+
+
+    def exec(self,QRCodeReaderRequest):
+        pass

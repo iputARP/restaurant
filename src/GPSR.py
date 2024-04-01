18e41b0a958c63865eb39698b91345638965e8b0
taishin <ok220259@oks.iput.ac.jp>
2024-04-01 13:32:11 +0900

    GPSRのメイン(smach)

---
+++ src/GPSR.py
@@ -0,0 +1,93 @@
+#! /usr/bin/env python3
+
+import smach
+import smach_ros
+import rospy
+class WAIT_DOOR_OPEN(smach.State):
+    def __init__(self):
+        smach.State.__init__(self, outcomes=['DoorOpen'])
+
+    def execute(self, ud):
+        int(input("type some key then door opened"))
+        return "DoorOpen"
+
+
+class MOVE_TO_INSTRUCTIONS(smach.State):
+    def __init__(self):
+        smach.State.__init__(self,outcomes=['MoveToInstructions','GPSREnd'])
+        self.counter=0
+
+    def execute(self, ud):
+        self.counter+=1
+        if self.counter==4:
+            return "GPSREnd"
+
+        int(input("type some key then move to instructions"))
+        return "MoveToInstructions"
+
+class UNDERSTANDING_COMMAND(smach.State):
+    def __init__(self):
+        smach.State.__init__(self,outcomes=['UnderstandingCommand'])
+
+    def execute(self, ud):
+        int(input("type some key then understanding command"))
+        return "UnderstandingCommand"
+
+
+class REPEAT_COMMAND(smach.State):
+    def __init__(self):
+        smach.State.__init__(self,outcomes=['ExecGrasp','ExecVision'])
+
+    def execute(self, ud):
+        key = int(input("type 0 or 1 key then finish repeat exec understood command 0:grasp,1:vision"))
+        if key==0:
+            return "ExecGrasp"
+        elif key==1:
+            return "ExecVision"
+
+
+class EXEC_GRASP(smach.State):
+    def __init__(self):
+        smach.State.__init__(self,outcomes=['FinishCommand'])
+
+    def execute(self, ud):
+        key = int(input("type some key then finish grasp"))
+        return "FinishCommand"
+
+class EXEC_VISION(smach.State):
+    def __init__(self):
+        smach.State.__init__(self,outcomes=['FinishCommand'])
+
+    def execute(self, ud):
+        key = int(input("type some key then finish visions"))
+        return "FinishCommand"
+
+
+
+if __name__ == "__main__":
+    rospy.init_node("GPSR")
+    print("main")
+
+    sm=smach.StateMachine(outcomes=["END"])
+
+    with(sm):
+        smach.StateMachine.add('WAIT_DOOR_OPEN',WAIT_DOOR_OPEN(),transitions={"DoorOpen":"MOVE_TO_INSTRUCTIONS"})
+        smach.StateMachine.add('MOVE_TO_INSTRUCTIONS',MOVE_TO_INSTRUCTIONS(),transitions={"MoveToInstructions":"UNDERSTANDING_COMMAND","GPSREnd":"END"})
+        smach.StateMachine.add('UNDERSTANDING_COMMAND',UNDERSTANDING_COMMAND(),transitions={"UnderstandingCommand":"REPEAT_COMMAND"})
+        smach.StateMachine.add('REPEAT_COMMAND',REPEAT_COMMAND(),transitions={"ExecGrasp":"EXEC_GRASP","ExecVision":"EXEC_VISION"})
+        smach.StateMachine.add('EXEC_GRASP',EXEC_GRASP(),transitions={"FinishCommand":"MOVE_TO_INSTRUCTIONS"})
+        smach.StateMachine.add('EXEC_VISION',EXEC_VISION(),transitions={"FinishCommand":"MOVE_TO_INSTRUCTIONS"})
+
+    sis=smach_ros.IntrospectionServer("server_name",sm,"/START")
+
+
+
+
+    sis.start()
+    # rospy.sleep(1)
+
+    result=sm.execute()
+    rospy.loginfo("result {}".format(result))
+    # rospy.spin()
+    sis.stop()
+    print("処理終了")

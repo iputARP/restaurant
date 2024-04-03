eeb58834249b44b6c00fbd3e3d357445fea3b7a2
higashi9 <ok230048@oks.iput.ac.jp>
2024-04-04 04:58:42 +0900

    東作成しました

--- src/qr_reader_server.py
+++ src/qr_reader_server.py
@@ -3,6 +3,7 @@
 import rospy
 from std_srvs.srv import Trigger,TriggerResponse
 import cv2
+import time
 from cv_bridge import CvBridge
 from sensor_msgs.msg import Image
 from image import ImageData
@@ -21,6 +22,7 @@ class QR_Reader(ImageData):
 
     def callback(self,msg):
         self.image_data=msg
+        self.cv_image_data = self.bridge.imgmsg_to_cv2(self.image_data)
 
     # 仕様:
     # 5秒間実行。５秒立っても認識できない場合、falseを返すようにする。
@@ -28,15 +30,45 @@ class QR_Reader(ImageData):
     def exec(self,req):
         # rosのsensor_msgs/Image型のデータ(image_data)をcv2で処理できる型に変換
         #cv_image_dataを使って、qr_codeを読むやつを作成してください。
-        cv_image_data = self.bridge.imgmsg_to_cv2(self.image_data)
+        # cv_image_data = self.bridge.imgmsg_to_cv2(self.image_data)
 
         # qrのデータ保存用
         qr_recog_data = None
 
         # 以下東くん作成
         try:
+            qcd = cv2.QRCodeDetector()
+            bridge = CvBridge()
+            couter = 0
+            true = 0
+            delay = 5
+            start_time = time.time()
+            while True:
+                ret_qr, decoded_info, points, _ = qcd.detectAndDecodeMulti(self.cv_image_data)
+                if ret_qr:
+                    for scan, p in zip(decoded_info, points):
+                        if scan:
+                            if couter < 5:
+                                print(scan)
+                                true = scan
+                                couter += 1
+                            color = (0, 255, 0)
+                        else:
+                            color = (0, 0, 255)
+
+                    elapsed_time = time.time() - start_time
+                    if elapsed_time >= delay:  # タイムアウト時間に達した場合
+                        if true:
+                            print("Timeout")
+                            return QRCodeReaderResponse(True,scan)
+                        else:
+                            print("QR code not detected.")
+                            return QRCodeReaderResponse(False,"よめませんでした")
+                        break
+
             rospy.loginfo("qr_reader_server exec")
-            return QRCodeReaderResponse(True,qr_recog_data)
+
+
         except Exception as e:
             return QRCodeReaderResponse(False, f"failed recognition {e}")
 

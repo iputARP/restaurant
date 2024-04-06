1dbd1764d244b1c63ad8699242e4ae1d560fb494
taishin <ok220259@oks.iput.ac.jp>
2024-04-06 11:00:15 +0900

    delete

--- src/process.py
+++
@@ -1,6 +0,0 @@
-#! /usr/bin/env python3
-import rospy
-import area
-import instruction
-if __name__ == "__main__":
-    print(instruction.Instruction.InstructionPoints["point0"])
--- src/process1.py
+++
@@ -1,8 +0,0 @@
-#! /usr/bin/env python3
-
-import rospy
-
-if __name__=="__main__":
-    rospy.init_node("process1")
-    while True:
-        rospy.loginfo("world")

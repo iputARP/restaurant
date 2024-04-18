c1177c3ac10081164539c56b5dd09592e5fa242e
taishin <ok220259@oks.iput.ac.jp>
2024-04-18 17:38:45 +0900

    座標値変更

--- src/area.py
+++ src/area.py
@@ -3,9 +3,14 @@ import math
 class Area:
     task_space = {
         "tidy_area":[1.69,1.13,math.pi/2],
+        "tidy_area.":[1.69,1.13,math.pi/2],
         "food":[2.98,0.84,-math.pi/2],
-        "shelf area":[4.03,3.83,math.pi/2],
-        "human area":[3.46,4.01,math.pi],
+        "food.": [2.98, 0.84, -math.pi / 2],
+        "shelf_area":[3.5,3.83,math.pi/2],
+        "shelf_area.": [4.03, 3.83, math.pi / 2],
+        "human_area":[3.46,4.01,math.pi],
+        "human_area.": [3.46, 4.01, math.pi],
         "living": [1.69, 1.13, math.pi/2],
+        "living.": [1.69, 1.13, math.pi / 2],
     }
 
